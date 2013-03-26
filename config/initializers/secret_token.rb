88a30fb9f8645256a458624b71d10192284a9bda
Steven Leung <sdklng@yahoo.com>
2013-03-26 16:45:07 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postandleave::Application.config.secret_token = '7aa9b35aae096a3cd7ef0cc099448931e524abc0e85970c684acc043e8097d4712f2b1aa571574bc4efb15a7990436cd6226bb666001c02b53e29586360518d5'
